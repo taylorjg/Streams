cad30ab75488e3f56cd1d60c8572f37dfdb17fb1
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-08-08 13:36:30 +0100

    Started work om standard query operators

---
+++ Streams/StreamLinqOperators.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Streams
+{
+    // 26.7.2 The Query Expression Pattern
+    // https://msdn.microsoft.com/en-us/library/bb308966.aspx#csharp3.0overview_topic19
+
+    public static class StreamLinqOperators
+    {
+        public static Stream<TSource> Where<TSource>(this Stream<TSource> source, Func<TSource, bool> predicate)
+        {
+            while (!source.IsEmpty)
+            {
+                if (predicate(source.Head))
+                    return Stream<TSource>.Cons(source.Head, () => source.Tail.Where(predicate));
+                source = source.Tail;
+            }
+
+            return Stream<TSource>.EmptyStream;
+        }
+
+        public static Stream<TSource> Where<TSource>(this Stream<TSource> source, Func<TSource, int, bool> predicate)
+        {
+            return WhereHelper(source, predicate, 0);
+        }
+
+        private static Stream<TSource> WhereHelper<TSource>(this Stream<TSource> source, Func<TSource, int, bool> predicate, int index)
+        {
+            while (!source.IsEmpty)
+            {
+                if (predicate(source.Head, index++))
+                    return Stream<TSource>.Cons(source.Head, () => source.Tail.WhereHelper(predicate, index));
+                source = source.Tail;
+            }
+
+            return Stream<TSource>.EmptyStream;
+        }
+
+        public static Stream<TResult> Select<TSource, TResult>(this Stream<TSource> source, Func<TSource, TResult> selector)
+        {
+            while (!source.IsEmpty)
+            {
+                return Stream<TResult>.Cons(selector(source.Head), () => source.Tail.Select(selector));
+            }
+
+            return Stream<TResult>.EmptyStream;
+        }
+
+        public static Stream<TResult> Select<TSource, TResult>(this Stream<TSource> source, Func<TSource, int, TResult> selector)
+        {
+            return SelectHelper(source, selector, 0);
+        }
+
+        private static Stream<TResult> SelectHelper<TSource, TResult>(this Stream<TSource> source, Func<TSource, int, TResult> selector, int index)
+        {
+            while (!source.IsEmpty)
+            {
+                return Stream<TResult>.Cons(selector(source.Head, index++), () => source.Tail.SelectHelper(selector, index));
+            }
+
+            return Stream<TResult>.EmptyStream;
+        }
+
+        public static Stream<TSource> Concat<TSource>(this Stream<TSource> first, Stream<TSource> second)
+        {
+            while (!first.IsEmpty)
+            {
+                return Stream<TSource>.Cons(first.Head, () => first.Tail.Concat(second));
+            }
+
+            while (!second.IsEmpty)
+            {
+                return Stream<TSource>.Cons(second.Head, () => second.Tail.Concat(Stream<TSource>.EmptyStream));
+            }
+
+            return Stream<TSource>.EmptyStream;
+        }
+    }
+}
---
+++ UnitTests/StreamLinqOperatorsTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Streams;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class StreamLinqOperatorsTests
+    {
+        [Test]
+        public void Where_MethodSyntax()
+        {
+            var stream1 = Enumerable.Range(1, 10).AsStream();
+            var stream2 = stream1.Where(IsEven);
+            Assert.That(stream2.AsEnumerable(), Is.EqualTo(new[] {2, 4, 6, 8, 10}));
+        }
+
+        [Test]
+        public void WhereWithIndex_MethodSyntax()
+        {
+            var stream1 = Enumerable.Range(1, 10).AsStream();
+            var tuples = new List<Tuple<int, int>>();
+            var stream2 = stream1.Where((n, index) =>
+            {
+                tuples.Add(Tuple.Create(n, index));
+                return IsEven(n);
+            });
+            Assert.That(stream2.AsEnumerable(), Is.EqualTo(new[] { 2, 4, 6, 8, 10 }));
+            Assert.That(tuples, Is.EqualTo(new[]
+            {
+                Tuple.Create(1, 0),
+                Tuple.Create(2, 1),
+                Tuple.Create(3, 2),
+                Tuple.Create(4, 3),
+                Tuple.Create(5, 4),
+                Tuple.Create(6, 5),
+                Tuple.Create(7, 6),
+                Tuple.Create(8, 7),
+                Tuple.Create(9, 8),
+                Tuple.Create(10, 9)
+            }));
+        }
+
+        [Test]
+        public void Where_QuerySyntax()
+        {
+            var stream1 = Enumerable.Range(1, 10).AsStream();
+            var stream2 = from n in stream1 where IsEven(n) select n;
+            Assert.That(stream2.AsEnumerable(), Is.EqualTo(new[] {2, 4, 6, 8, 10}));
+        }
+
+        [Test]
+        public void Select_MethodSyntax()
+        {
+            var stream1 = Enumerable.Range(1, 10).AsStream();
+            var stream2 = stream1.Select(n => string.Format("{0}", n));
+            Assert.That(stream2.AsEnumerable(), Is.EqualTo(new[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10" }));
+        }
+
+        [Test]
+        public void SelectWithIndex_MethodSyntax()
+        {
+            var stream1 = Enumerable.Range(1, 10).AsStream();
+            var tuples = new List<Tuple<string, int>>();
+            var stream2 = stream1.Select((n, index) =>
+            {
+                var s = string.Format("{0}", n);
+                tuples.Add(Tuple.Create(s, index));
+                return s;
+            });
+            Assert.That(stream2.AsEnumerable(), Is.EqualTo(new[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10" }));
+            Assert.That(tuples, Is.EqualTo(new[]
+            {
+                Tuple.Create("1", 0),
+                Tuple.Create("2", 1),
+                Tuple.Create("3", 2),
+                Tuple.Create("4", 3),
+                Tuple.Create("5", 4),
+                Tuple.Create("6", 5),
+                Tuple.Create("7", 6),
+                Tuple.Create("8", 7),
+                Tuple.Create("9", 8),
+                Tuple.Create("10", 9)
+            }));
+        }
+
+        [Test]
+        public void Select_QuerySyntax()
+        {
+            var stream1 = Enumerable.Range(1, 10).AsStream();
+            var stream2 = from n in stream1 select string.Format("{0}", n);
+            Assert.That(stream2.AsEnumerable(), Is.EqualTo(new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" }));
+        }
+
+        [TestCase(new[] {1, 2, 3}, new[] {4, 5, 6}, new[] {1, 2, 3, 4, 5, 6})]
+        [TestCase(new[] {1, 2, 3}, new int[0], new[] {1, 2, 3})]
+        [TestCase(new int[0], new[] {4, 5, 6}, new[] {4, 5, 6})]
+        public void Concat(int[] first, int[] second, int[] expected)
+        {
+            var stream1 = first.AsStream();
+            var stream2 = second.AsStream();
+            var actual = stream1.Concat(stream2);
+            Assert.That(actual.AsEnumerable(), Is.EqualTo(expected));
+        }
+
+        private static bool IsEven(int n)
+        {
+            return n % 2 == 0;
+        }
+    }
+}
